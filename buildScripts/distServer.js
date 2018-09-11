c66702c57890c3413e10788cd0004adb4aa268dc
Krishna Ramire <kvramire@gmail.com>
2018-09-11 07:45:41 +0530

    minified

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -4,28 +4,19 @@ import open from 'open';
 import compression from 'compression';
 
 /* eslint-disable no-console */
-const port = 3000;
+
+const port = 3002;
 const app = express();
 
-app.use(compression()); //gzip compression
+app.use(compression());
 app.use(express.static('dist'));
 
-
-app.get('/', function(req, res){
-  console.log('incoming request...');
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', (req, res)=>{
-  res.json([
-    {"id":1, "firstName": "Bob", "LastName":"Smith", "email":"bob@gmail.com"},
-    {"id":2, "firstName": "Tamy", "LastName":"Norton", "email":"tamy@gmail.com"},
-    {"id":3, "firstName": "Tina", "LastName":"Lee", "email":"tina@gmail.com"}
-  ]);
-});
-
-app.listen(port, function(err){
-  if(err){
+app.listen(port, function(err) {
+  if (err) {
     console.log(err);
   } else {
     open('http://localhost:' + port);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 
 export default {
   debug: true,
@@ -15,6 +17,22 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
     // Eliminate duplicate packages when generating bundle
     new webpack.optimize.DedupePlugin(),
     // Minify JS
